ea6e25a54fdea79d42b4498ba1a8b21377121621
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2024-11-02 21:38:25 +0200

    changes in book now button

--- src/Components/WhatsappButton.jsx
+++ src/Components/WhatsappButton.jsx
@@ -1,13 +1,24 @@
+import { t } from "i18next";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import ReactWhatsapp from "react-whatsapp";
 
-const WhatsappButton = ({ size }) => (
+const WhatsappButton = ({ size, button, text }) => (
   <ReactWhatsapp
     className={`text-green-500 ${size} hover:text-green-400`}
     number="+20 1095521277"
     message="Hello, is there anyone available to chat with!"
   >
-    <i className="fab fa-whatsapp"></i>
+    {button ? (
+      <button
+        type="button"
+        className="text-white  bg-gradient-to-r from-cyan-900 to-blue-900 hover:bg-gradient-to-r hover:from-cyan-700 hover:to-blue-700 px-5 py-2 border-none rounded-md mt-2 transition duration-500   hover:shadow-lg "
+      >
+        <Link>{t(text)}</Link>
+      </button>
+    ) : (
+      <i className="fab fa-whatsapp"></i>
+    )}
   </ReactWhatsapp>
 );
 
@@ -15,4 +26,6 @@ export default WhatsappButton;
 
 WhatsappButton.propTypes = {
   size: PropTypes.string,
+  button: PropTypes.string,
+  text: PropTypes.string,
 };
