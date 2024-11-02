eeb9b3ebe9d11bd46690cff97ddc427a10ed6736
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2024-11-02 14:32:43 +0200

    test #4

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,17 +1,14 @@
-// src/Layout.js
-import { Outlet } from "react-router-dom";
-import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import { PropTypes } from "prop-types";
 
 const Layout = ({ direction, setDirection }) => {
   return (
-    <div className="flex flex-col min-h-screen " >
+    <div className="flex flex-col min-h-screen ">
       <Navbar direction={direction} setDirection={setDirection} />
-      <main className="flex-grow">
+      {/* <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 };
