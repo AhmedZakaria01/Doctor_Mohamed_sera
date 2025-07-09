d890a9056035fb352f13b924c21380c359b2a903
Ahmed Zakaria <64384776+AhmedZakaria01@users.noreply.github.com>
2025-07-09 13:18:21 +0300

    scroll to top

--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -3,10 +3,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import { PropTypes } from "prop-types";
+import ScrollToTop from "../Components/ScrollToTop";
 
 const Layout = ({ direction, setDirection }) => {
   return (
-    <div className="flex flex-col min-h-screen " >
+    <div className="flex flex-col min-h-screen ">
+      <ScrollToTop />
+
       <Navbar direction={direction} setDirection={setDirection} />
       <main className="flex-grow">
         <Outlet />
